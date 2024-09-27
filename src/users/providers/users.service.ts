219bd486a3e3c14899435d14dae6ed5eb965dd2b
AHMEDNABIB <nabibpallab@gmail.com>
2024-09-27 10:30:51 +0600

    add circular dependency

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { AuthService } from 'src/auth/providers/auth.service';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 @Injectable()
 export class UsersService {
+  constructor(
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+  ) {}
+
   public findAll(
     getUserParamDto: GetUsersParamDto,
     limit: number,
     page: number,
   ) {
     console.log(limit, page);
+
+    const isAuth = this.authService.isAuth();
+    console.log(isAuth);
+
     return [
       {
         firstName: 'Ahmed Nabib',
