9cfc20941c146aca3b26d634c8e0a65975a22e93
AHMEDNABIB <nabibpallab@gmail.com>
2024-11-15 00:34:27 +0600

    Feat: add tag and meta-option entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
+import { TagsModule } from './tags/tags.module';
+import { MetaOptionsModule } from './meta-options/meta-options.module';
 
 @Module({
   imports: [
@@ -24,6 +26,8 @@ import { User } from './users/user.entity';
         database: 'nestjs-blog',
       }),
     }),
+    TagsModule,
+    MetaOptionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
