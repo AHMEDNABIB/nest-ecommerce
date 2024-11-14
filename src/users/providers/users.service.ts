f01aafeb368566ad64c7419a4db2169bbc0c1f32
AHMEDNABIB <nabibpallab@gmail.com>
2024-11-14 13:52:16 +0600

    add post and user entity

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,12 +1,33 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { AuthService } from 'src/auth/providers/auth.service';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dtos/create-user.dto';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
+import { User } from '../user.entity';
 @Injectable()
 export class UsersService {
   constructor(
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
+
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
+
+  public async createUser(createUserDto: CreateUserDto) {
+    // check if the user exist
+    const existingUser = await this.userRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    // Create a new User
+
+    let newuser = this.userRepository.create(createUserDto);
+    newuser = await this.userRepository.save(newuser);
+
+    return newuser
+  }
 
   public findAll(
     getUserParamDto: GetUsersParamDto,
